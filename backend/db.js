04720f2f4014b4643da0f4a1d4c91a3f405fecd6
Priyanka G <priyankagolani07@hotmail.com>
2025-08-12 18:20:41 +0530

    Generate JWT Token

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI =
-  "mongodb://localhost:27017/readPreference=primary&appName=MongoDB%20Compass&directConnection=true&ssl=false";
+const mongoURI = "mongodb://localhost:27017/inotebook";
 const connectToMongo = async () => {
   try {
     await mongoose.connect(mongoURI);
