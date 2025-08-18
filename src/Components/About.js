bcafc6dc6eb96a74d604be5b85c7824b09712302
Priyanka G <priyankagolani07@hotmail.com>
2025-08-18 13:29:32 +0530

    Till NoteItem

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,17 +1,5 @@
-import React, { useEffect, useContext } from "react";
-import noteContext from "../context/notes/NoteContext";
-
 const About = () => {
-  const a = useContext(noteContext);
-  useEffect(() => {
-    a.update();
-    //eslint-disable-next-line
-  }, []);
-  return (
-    <div>
-      This is About {a.state.name} and he is in class {a.state.class}
-    </div>
-  );
+  return <div>This is About page</div>;
 };
 
 export default About;
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,24 +2,75 @@ import { useState } from "react";
 import noteContext from "./NoteContext";
 
 const NoteState = (props) => {
-  const s1 = {
-    name: "Harry",
-    class: "5b",
-  };
+  const notesInitial = [
+    {
+      _id: "689f710e73935f9f1df1bb4d",
+      user: "689b29eb9a869c77094773f4",
+      title: "My Title",
+      description: "Please wake up early",
+      tag: "personal",
+      date: "2025-08-15T17:40:30.393Z",
+      __v: 0,
+    },
+    {
+      _id: "689f710f73935f9f1df1bb4f",
+      user: "689b29eb9a869c77094773f4",
+      title: "My Title",
+      description: "Please wake up early",
+      tag: "personal",
+      date: "2025-08-15T17:40:31.332Z",
+      __v: 0,
+    },
+    {
+      _id: "689f710e73935f9f1df1bb4d",
+      user: "689b29eb9a869c77094773f4",
+      title: "My Title",
+      description: "Please wake up early",
+      tag: "personal",
+      date: "2025-08-15T17:40:30.393Z",
+      __v: 0,
+    },
+    {
+      _id: "689f710f73935f9f1df1bb4f",
+      user: "689b29eb9a869c77094773f4",
+      title: "My Title",
+      description: "Please wake up early",
+      tag: "personal",
+      date: "2025-08-15T17:40:31.332Z",
+      __v: 0,
+    },
+    {
+      _id: "689f710e73935f9f1df1bb4d",
+      user: "689b29eb9a869c77094773f4",
+      title: "My Title",
+      description: "Please wake up early",
+      tag: "personal",
+      date: "2025-08-15T17:40:30.393Z",
+      __v: 0,
+    },
+    {
+      _id: "689f710f73935f9f1df1bb4f",
+      user: "689b29eb9a869c77094773f4",
+      title: "My Title",
+      description: "Please wake up early",
+      tag: "personal",
+      date: "2025-08-15T17:40:31.332Z",
+      __v: 0,
+    },
+    {
+      _id: "689f710e73935f9f1df1bb4d",
+      user: "689b29eb9a869c77094773f4",
+      title: "My Title",
+      description: "Please wake up early",
+      tag: "personal",
+      date: "2025-08-15T17:40:30.393Z",
+      __v: 0,
+    }
+  ];
 
-  const [state, setState] = useState(s1);
-  const update = () => {
-    setState(
-      {
-        name: "Larry",
-        class: "10b",
-      },
-      1000
-    );
-  };
-  
+  const [notes, setNotes] = useState(notesInitial);
   return (
-    <noteContext.Provider value={{ state, update }}>
+    <noteContext.Provider value={{ notes, setNotes }}>
       {props.children}
     </noteContext.Provider>
   );
